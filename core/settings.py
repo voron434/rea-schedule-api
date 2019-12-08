0b479a61f512d276d1adee949f16408b26d24e76
Starcoding <fagost32@gmail.com>
2019-12-08 17:38:47 +0300

    Added token giving and views for api

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'REPLACE_ME')
 
 DEBUG = env.bool("DEBUG", True)
 
-ALLOWED_HOSTS = [os.getenv('PROD_HOST', 'localhost'), ]
+ALLOWED_HOSTS = [os.getenv('PROD_HOST', 'localhost'), "127.0.0.1"]
 
 
 INSTALLED_APPS = [
--- schedule/urls.py
+++ schedule/urls.py
@@ -4,9 +4,10 @@ from schedule import views
 
 
 urlpatterns = [
-    path('groups_list/', views.groups_list),
+    path('api/groups_list/', views.groups_list),
     path('facultys_list/', views.facultys_list),
     path('list_shedule/',views.list_shedule),
     path('courses_list/', views.courses_list),
     path('groups_by_course/', views.groups_by_course),
+    path('api/login', views.login),
 ]
