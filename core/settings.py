42aa5e9fc3560af4e11187391fd0adfd37226aa7
Starcoding <fagost32@gmail.com>
2019-12-10 17:47:54 +0300

    Added token auth, and removed basic auth

--- core/settings.py
+++ core/settings.py
@@ -42,8 +42,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
