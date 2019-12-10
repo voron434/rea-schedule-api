5d967edb8be7d046c1c6f6e75003d6fb29954c0f
voron434 <070809010@mail.ru>
2019-12-10 15:29:01 +0300

    Made script more stable

--- schedule/management/commands/parse_xml.py
+++ schedule/management/commands/parse_xml.py
@@ -22,17 +22,29 @@ class Command(BaseCommand):
     @transaction.non_atomic_requests
     def handle(self, *args, **options):
         dump_path = options['path'] if 'path' in options.keys() else XML_PATH
-        models.Lesson.objects.all().delete()
         if dump_path and os.path.exists(dump_path):
-            filenames = [join(dump_path, f) for f in os.listdir(dump_path) if isfile(join(dump_path, f))]
+            filepaths = [
+                join(dump_path, f)
+                for f in os.listdir(dump_path)
+                if isfile(join(dump_path, f)) and f.endswith('.xml')
+            ]
         else:
-            self.stdout.write("Not able to find path to parse. Please, Specify it in settings.py or pass it as argument --path.")
+            self.stdout.write("Not able to find path to parse."
+                              "Please, Specify it in settings.py "
+                              "or pass it as argument --path.")
             return
-        # clear db?
+        if not filepaths:
+            self.stdout.write("Path passed into argument --path has no .xml files")
+        models.Lesson.objects.all().delete()  # delete only if files found
 
-        for index, filename in enumerate(filenames):
-            self._parse_xml_schedule(filename)
-            self.stdout.write(self.style.SUCCESS(f'Successfully parsed {index}/{len(filenames)}, {filename}'))
+        for index, filepath in enumerate(filepaths, 1):
+            self._parse_xml_schedule(filepath)
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Successfully parsed {index}/{len(filepaths)}, {filepath}'
+                )
+            )
+            os.remove(filepath)
 
     def _parse_xml_schedule(self, path):
         xml_root = ET.parse(path).getroot()
@@ -54,14 +66,13 @@ class Command(BaseCommand):
                 subgroup_num = lesson[2].text or None
                 lesson_title = lesson[3].text
                 lesson_type = lesson[4].text
-                
+
                 room_num = lesson[5].text
                 room_campus = lesson[7].text
                 classroom, is_created = models.Сlassroom.objects.get_or_create(campus=room_campus, room_num=room_num)
 
                 teacher_name = lesson[6].text
                 teacher, is_created = models.Teacher.objects.get_or_create(name=teacher_name)
-                
 
                 lesson = models.Lesson(
                     teacher=teacher,
